3234dd03b367d0168b749f18a2ec941ba8f6accf
code-by-mandy <mandycodes2@gmail.com>
2021-04-26 16:44:32 -0400

    toggle nav active class success

--- script/script.js
+++ script/script.js
@@ -1,28 +1,52 @@
 const portfolio = {};
 
 portfolio.init = () => {
+    portfolio.menu = document.querySelectorAll(".menu");
+    portfolio.pages = document.querySelectorAll("#home, #about, #projects, #contact");
+
     portfolio.toggleMain();
+    portfolio.clearActiveNav();
 }
 
 /*toggle visibility of page when clicked in nav*/
 portfolio.toggleMain = () => {
-
-    const menu = document.querySelectorAll(".menu")
-    const pages = document.querySelectorAll("#home, #about, #projects, #contact")
    
-    menu.forEach ( menuItem => {
+    portfolio.menu.forEach ( menuItem => {
         menuItem.addEventListener('click', function() {
-            let menuID = this.hash;
-            let menuIDNoHash = menuID.slice(1);
-            pages.forEach (page => {                
-                if (menuIDNoHash === page.id) {
+            const chosenID = this.hash;
+            const chosenIDNoHash = chosenID.slice(1);
+            portfolio.pages.forEach (page => {                
+                if (chosenIDNoHash === page.id) {
                     page.classList.remove("sr-only");
+                    portfolio.chosenPageID = page.id;
                 } else {
                     page.classList.add("sr-only");
                 }
-            })
+            });
+            portfolio.chosenPage = this;
         });
     })
+}
+
+/*toggle active class on nav*/
+portfolio.clearActiveNav = () => {
+
+    const allNav = document.querySelector('nav');
+
+    allNav.addEventListener('click', function() {
+        portfolio.menu.forEach ( menuItem => {
+            if (menuItem === portfolio.chosenPage) {
+                menuItem.classList.add("active");
+            } else {
+                menuItem.classList.remove("active");
+            }
+        })
+    })
+}
+
+/*change mobile menu style based on page*/
+portfolio.menuStyle = () => {
+
 }
 
 portfolio.init();
