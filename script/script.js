202db45c47957670638aa119997fc7fb882e4470
code-by-mandy <mandycodes2@gmail.com>
2021-04-28 12:29:47 -0400

    toggle minimized menu success

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ const portfolio = {};
 portfolio.init = () => {
 
     portfolio.allNav = document.querySelector('nav');
+    portfolio.menuList = document.querySelector('.pop_menu')
     portfolio.menu = document.querySelectorAll(".menu");
     portfolio.pages = document.querySelectorAll("#home, #about, #projects, #contact");
 
@@ -11,7 +12,7 @@ portfolio.init = () => {
     portfolio.minMenu();
 }
 
-/*toggle visibility of page when clicked in nav*/
+/*toggle visibility of page when clicked in nav, close popup nav when page is chosen (line 29)*/
 portfolio.toggleMain = () => {
    
     portfolio.menu.forEach ( menuItem => {
@@ -25,6 +26,7 @@ portfolio.toggleMain = () => {
                 if (chosenIDNoHash === page.id) {
                     page.classList.remove("sr-only");
                     portfolio.chosenPageID = page.id;
+                    portfolio.menuList.classList.remove("opened");
                 } else {
                     page.classList.add("sr-only");
                 }
@@ -35,19 +37,26 @@ portfolio.toggleMain = () => {
 
 /*toggle active class on nav*/
 portfolio.clearActiveNav = () => {
-
-
     portfolio.allNav.addEventListener('click', function() {
         portfolio.menu.forEach ( menuItem => {
             if (menuItem === portfolio.chosenPage) {
-                menuItem.classList.add("active");
+                menuItem.classList.add("active");                
             } else {
                 menuItem.classList.remove("active");
             }
-        })
+        });
 
         portfolio.menuStyle();
     })
+}
+
+
+/*show minimized menu */
+portfolio.minMenu = () => {
+    const openMenu = document.querySelector('.open_menu');
+    openMenu.addEventListener('click', function() {
+        portfolio.menuList.classList.add("opened");
+    });
 }
 
 /*change mobile menu style based on page*/
@@ -55,19 +64,13 @@ portfolio.menuStyle = () => {
     
     //toggle css to show all if home  
     if (portfolio.chosenPageID !== "home") {
+        portfolio.allNav.classList.add("minimized");
         portfolio.allNav.classList.remove("home");
     } else {
         portfolio.allNav.classList.add("home");
+        portfolio.allNav.classList.remove("minimized");
+        
     }
-}
-
-/*minimize menu*/
-portfolio.minMenu = () => {
-    const openMenu = document.querySelector('.open_menu');
-    const menuList = document.querySelector('.pop_menu')
-    openMenu.addEventListener('click', function() {
-        menuList.classList.add("opened");
-    });
 }
 
 portfolio.init();
