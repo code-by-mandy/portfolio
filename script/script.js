c20657cba9f9e29992209b54b4c3bc095af3ee95
code-by-mandy <mandycodes2@gmail.com>
2021-05-04 14:32:49 -0400

    touch nav bug fixed

--- script/script.js
+++ script/script.js
@@ -7,15 +7,18 @@ portfolio.init = () => {
     portfolio.pages = document.querySelectorAll("#home, #about, #projects, #contact");
     portfolio.tabMsg = document.querySelector(".tabMsg");
 
+    /*namespace functions - details below*/
     portfolio.scrolledNav();
     portfolio.keyboardNav();
+    portfolio.touchNav();
     portfolio.formSubmit();
-
 }
+
 
 /*change active on scroll*/
 portfolio.scrolledNav = () => {
     
+    /*check whether target element is within view*/
     const checkInView = (elem) => {
         const checkElemView = elem.getBoundingClientRect();
         return (
@@ -24,6 +27,7 @@ portfolio.scrolledNav = () => {
             );
     };
 
+    /*on scroll, if page is within view, define element as partfolio.inview variable*/
     window.addEventListener('scroll', function () {
         portfolio.pages.forEach ( page => {
             if (checkInView(page)) {
@@ -31,22 +35,31 @@ portfolio.scrolledNav = () => {
             }
         });
 
+        /*for each menu item, if menu hash is same as page id, add active line to menu*/
         portfolio.menuItems.forEach ( menuItem => {
             const viewID = menuItem.hash;
             const viewIDNoHash = viewID.slice(1);
 
             if (viewIDNoHash === portfolio.inView.id) {
                 menuItem.classList.add("active");
-                portfolio.activePage = menuItem;   
             } else {
                 menuItem.classList.remove("active");
-            }
-        });
-    }, false);
+            }            
+        });        
+    });
 }
 
-portfolio.keyboardNav = () => {    
+/*tie touch and mouse to solve multiple active nav issue when scrolling with touch*/
+portfolio.touchNav = () => {
+    document.addEventListener("touchend", function(e) {
+        let touched = e.target;
+        touched.click();
+        e.preventDefault();
+    })
+}
 
+/*tip for keyboard users on using F4 to quickly toggle menu*/
+portfolio.keyboardNav = () => {    
     document.addEventListener("keydown", function(e) {
         if (e.code === "F4") {
             portfolio.activePage.focus();
@@ -54,6 +67,7 @@ portfolio.keyboardNav = () => {
     });
 }
 
+/*disable and clear contact form on submit*/
 portfolio.formSubmit = () => {
     const submit = document.querySelector("button");
     const formMsg = document.querySelector(".formMsg");
